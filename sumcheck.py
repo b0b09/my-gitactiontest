a5017ae01fd53ca80f2b4ad148eb2f53edbfdc8e
b0b09 <142707485+b0b09@users.noreply.github.com>
2026-09-24 11:30:38 +0530

    Update sumcheck.py

--- sumcheck.py
+++ sumcheck.py
@@ -1,4 +1,4 @@
 a = int(input("Enter value for a"))
-b = int(input("Enter value for b")
-c = a + b;
-print("sum of a and b",c);
+b = int(input("Enter value for b"))
+c = a + b
+print("sum of a and b",c)
